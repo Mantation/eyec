71b43c124b1ae7cd62a2761add25d671d07660b1
Mantation <rpholose@gmail.com>
2019-06-03 22:21:18 +0200

    modified heroku settings

--- eyec/settings/base.py
+++ eyec/settings/base.py
@@ -147,21 +147,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
-#STATICFILES = (
-#    os.path.join(BASE_DIR,'static'),
-#)
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(_file_))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES = (
+    os.path.join(BASE_DIR,'static'),
+)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(_file_))
+
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
