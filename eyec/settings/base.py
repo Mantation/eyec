40016c9a4bd3e83e1ef0090d290360bd3775ecdc
Mantation <rpholose@gmail.com>
2019-06-03 23:01:40 +0200

    initial commit

--- eyec/settings/base.py
+++ eyec/settings/base.py
@@ -168,7 +168,7 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR
